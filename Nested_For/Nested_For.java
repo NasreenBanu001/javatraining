e6b3c1750f819e67e42f36d5944b13555487d88e
NasreenBanu001 <s.nasreenbanu001@gmail.com>
2023-06-07 14:12:27 +0530

    Looping statment

---
+++ Nested_For/Nested_For.java
@@ -0,0 +1,20 @@
+package basic.Nested_For;
+
+import java.util.Scanner;
+
+public class Nested_For {
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the limit:");
+        int limit= in.nextInt();
+        for (int i=1;i<=limit;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+
+
+}
